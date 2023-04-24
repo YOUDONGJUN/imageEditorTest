40882641829a1fd17560faa5cfe9408e0e88210a
YOUDONGJUN\ehdwn <ehdwnsdb@naver.com>
2023-04-24 16:56:46 +0900

    230424 16:56

--- vue-project/src/components/Test.vue
+++ vue-project/src/components/Test.vue
@@ -7,22 +7,27 @@ import 'tui-color-picker/dist/tui-color-picker.css';
 import 'tui-image-editor/dist/tui-image-editor.css';
 import {ImageEditor} from '@toast-ui/vue-image-editor';
 
+const FileSaver = require('file-saver');
 export default {
   components: {
     'tui-image-editor': ImageEditor,
   },
 
   data() {
+    console.log(FileSaver);
     return {
       useDefaultUI: true,
       options: {
         // for tui-image-editor component's "options" prop
         cssMaxWidth: 700,
         cssMaxHeight: 500,
+
       },
+
     };
   },
 };
+
 </script>
 
 <!--<style>-->
